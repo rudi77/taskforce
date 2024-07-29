e7f8c4ba1fdf9094bb3923e08e55d507844b932f
rudi77 <rudi.dittrich77@gmail.com>
2024-07-29 14:31:44 +0200

    #2 implemented CoT strategy

---
+++ src/Taskforce/Planning/Strategy/ChainOfThoughtStrategy.cs
@@ -0,0 +1,26 @@
+﻿using Taskforce.Abstractions;
+
+namespace Planning.Strategy
+{
+    public class ChainOfThoughtStrategy : IPlanningStrategy
+    {
+        public async Task<List<string>> PlanAsync(string userPrompt, ILLM llm, string generalInstruction, string answerInstruction)
+        {
+            var questionAnswerPromptPart = $"USER QUESTION\n{userPrompt}\n{answerInstruction}";
+            var response = await llm.SendMessageAsync(generalInstruction, questionAnswerPromptPart);
+            var questions = Newtonsoft.Json.JsonConvert.DeserializeObject<Questions>(response.ToString());
+
+            return questions.SubQuestions;
+        }
+
+        public async Task<List<string>> PlanAsync(string userPrompt, IList<string> imagePaths, ILLM llm, string generalInstruction, string answerInstruction)
+        {
+            var questionAnswerPromptPart = $"USER QUESTION\n{userPrompt}\n{answerInstruction}";
+            var response = await llm.SendMessageAsync(generalInstruction, questionAnswerPromptPart, imagePaths);
+            var questions = Newtonsoft.Json.JsonConvert.DeserializeObject<Questions>(response.ToString());
+
+            return questions.SubQuestions;
+        }
+    }
+
+}
---
+++ src/Taskforce/Taskforce.Abstractions/IPlanningStrategycs.cs
@@ -0,0 +1,9 @@
+﻿namespace Taskforce.Abstractions
+{
+    public interface IPlanningStrategy
+    {
+        Task<List<string>> PlanAsync(string userPrompt, ILLM llm, string generalInstruction, string answerInstruction);
+
+        Task<List<string>> PlanAsync(string userPrompt, IList<string> imagePaths, ILLM llm, string generalInstruction, string answerInstruction);
+    }
+}
